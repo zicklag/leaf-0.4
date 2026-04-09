4a1dd7ee0a2d1aa76d22f7ca8dfdf7ed2a228205
Zicklag <zicklag@katharostech.com>
2026-04-09 08:41:12 -0500

    feat: implement arbiter core with model-based testing.

--- crates/arbiter-core/src/lib.rs
+++ crates/arbiter-core/src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-  collections::{HashMap, HashSet},
+  collections::{BTreeMap, BTreeSet},
   hash::Hash,
 };
 
@@ -16,23 +16,23 @@ pub type SpaceKey = String;
 
 #[derive(Default, Deserialize, Clone, PartialEq, Eq, Debug)]
 pub struct ArbiterService {
-  arbiters: HashMap<ArbiterDid, Arbiter>,
+  arbiters: BTreeMap<ArbiterDid, Arbiter>,
 }
 
 #[derive(Deserialize, Clone, PartialEq, Eq, Debug)]
 pub struct Arbiter {
   pub did: ArbiterDid,
-  pub admins: HashSet<UserDid>,
-  pub roles: HashMap<RoleId, HashSet<UserDid>>,
-  pub spaces: HashMap<SpaceKey, PermSpace>,
+  pub admins: BTreeSet<UserDid>,
+  pub roles: BTreeMap<RoleId, BTreeSet<UserDid>>,
+  pub spaces: BTreeMap<SpaceKey, PermSpace>,
 }
 
-#[derive(Deserialize, Clone, PartialEq, Eq, Debug)]
+#[derive(Deserialize, Clone, PartialEq, Eq, Debug, Default)]
 pub struct PermSpace {
-  pub members: HashMap<Member, Access>,
+  pub members: BTreeMap<Member, Access>,
 }
 
-#[derive(Deserialize, Hash, PartialEq, Eq, Clone, Debug)]
+#[derive(Deserialize, Hash, PartialEq, Eq, Clone, Debug, PartialOrd, Ord)]
 #[serde(tag = "tag", content = "value")]
 pub enum Member {
   User(UserDid),
@@ -48,7 +48,7 @@ pub enum Access {
 
 impl ArbiterService {
   pub fn create(&mut self, user_did: UserDid, arbiter_did: ArbiterDid) -> anyhow::Result<()> {
-    if !self.arbiters.contains_key(&arbiter_did) {
+    if self.arbiters.contains_key(&arbiter_did) {
       anyhow::bail!("Arbiter already exists");
     }
 
@@ -56,12 +56,171 @@ impl ArbiterService {
       arbiter_did.clone(),
       Arbiter {
         did: arbiter_did,
-        admins: HashSet::from_iter([user_did]),
-        roles: HashMap::default(),
-        spaces: HashMap::default(),
+        admins: BTreeSet::from_iter([user_did]),
+        roles: BTreeMap::default(),
+        spaces: BTreeMap::default(),
       },
     );
 
+    Ok(())
+  }
+
+  pub fn add_admin(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    new_admin: UserDid,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    arbiter.admins.insert(new_admin);
+
+    Ok(())
+  }
+
+  pub fn remove_admin(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    admin: UserDid,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    if !arbiter.admins.remove(&admin) {
+      anyhow::bail!("User is not an admin");
+    }
+    Ok(())
+  }
+
+  pub fn create_space(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    space_key: SpaceKey,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    if arbiter.spaces.contains_key(&space_key) {
+      anyhow::bail!("Space already exists");
+    }
+    arbiter.spaces.insert(space_key, PermSpace::default());
+    Ok(())
+  }
+
+  pub fn set_space_member_access(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    space_key: SpaceKey,
+    member: Member,
+    access: Access,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    let Some(space) = arbiter.spaces.get_mut(&space_key) else {
+      anyhow::bail!("Space does not exist");
+    };
+    space.members.insert(member, access);
+    Ok(())
+  }
+
+  pub fn remove_space_member(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    space_key: SpaceKey,
+    member: Member,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    let Some(space) = arbiter.spaces.get_mut(&space_key) else {
+      anyhow::bail!("Space does not exist");
+    };
+    if space.members.remove(&member).is_none() {
+      anyhow::bail!("`{member:?}` was not a member of space `{space_key}`");
+    }
+    Ok(())
+  }
+
+  pub fn create_role(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    role_id: RoleId,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    if arbiter.roles.contains_key(&role_id) {
+      anyhow::bail!("Role already exists");
+    }
+    arbiter.roles.insert(role_id, Default::default());
+
+    Ok(())
+  }
+
+  pub fn add_role_member(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    role_id: RoleId,
+    member: UserDid,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    let Some(role) = arbiter.roles.get_mut(&role_id) else {
+      anyhow::bail!("Role already exists");
+    };
+    role.insert(member);
+    Ok(())
+  }
+
+  pub fn remove_role_member(
+    &mut self,
+    user_did: UserDid,
+    arbiter_did: ArbiterDid,
+    role_id: RoleId,
+    member: UserDid,
+  ) -> anyhow::Result<()> {
+    let Some(arbiter) = self.arbiters.get_mut(&arbiter_did) else {
+      anyhow::bail!("Arbiter does not exist");
+    };
+    if !arbiter.admins.contains(&user_did) {
+      anyhow::bail!("User must be admin to add admin");
+    }
+    let Some(role) = arbiter.roles.get_mut(&role_id) else {
+      anyhow::bail!("Role already exists");
+    };
+    if !role.remove(&member) {
+      anyhow::bail!("Member `{member}` not in role `{role_id}`");
+    }
     Ok(())
   }
 }
--- crates/arbiter-core/src/mbt.rs
+++ crates/arbiter-core/src/mbt.rs
@@ -11,17 +11,25 @@ impl State<ArbiterService> for ArbiterService {
 impl Driver for super::ArbiterService {
   type State = ArbiterService;
 
-  #[allow(unused)]
+  #[allow(unused, non_snake_case)]
   fn step(&mut self, step: &Step) -> Result {
     switch!(step {
-        init => *self == Default::default(),
-        create(user_did, arbiter_did) => self.create(user_did, arbiter_did),
+        init => std::mem::take(self),
+        createArbiterAny(userDid, arbiterDid) => self.create(userDid, arbiterDid)?,
+        addArbiterAdminAny(userDid, arbiterDid, newAdminDid) => self.add_admin(userDid, arbiterDid, newAdminDid)?,
+        removeArbiterAdminAny(userDid, arbiterDid, removedAdmin) => self.remove_admin(userDid, arbiterDid, removedAdmin)?,
+        createSpaceAny(userDid, arbiterDid, spaceKey) => self.create_space(userDid, arbiterDid, spaceKey)?,
+        setSpaceMemberAccessAny(userDid, arbiterDid, spaceKey, member, access) => self.set_space_member_access(userDid, arbiterDid, spaceKey, member, access)?,
+        removeSpaceMemberAny(userDid, arbiterDid, spaceKey, member) => self.remove_space_member(userDid, arbiterDid, spaceKey, member)?,
+        createRoleAny(userDid, arbiterDid, roleId) => self.create_role(userDid, arbiterDid, roleId)?,
+        addRoleMemberAny(userDid, arbiterDid, roleId, member) => self.add_role_member(userDid, arbiterDid, roleId, member)?,
+        removeRoleMemberAny(userDid, arbiterDid, roleId, member) => self.remove_role_member(userDid, arbiterDid, roleId, member)?,
     })
   }
 }
 
 // Run multiple traces in simulation mode
-#[quint_run(spec = "../spec/arbiter.qnt")]
+#[quint_run(spec = "./spec/arbiter.qnt", max_samples = 500)]
 fn simulation() -> impl Driver {
   ArbiterService::default()
 }
